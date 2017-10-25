1745e87614bc071d5dc94a2475164f6ea9285113
Khojanator <ahsan.khoja@gmail.com>
2017-10-24 21:43:59 -0400

    Parts 1-3 completed; Yayy!

--- assignment7.hs
+++ assignment7.hs
@@ -1,1 +1,24 @@
 -- Ahsan Ali Khoja & Rei Rembeci
+
+import System.Random
+
+-- Hello, World
+helloWorld :: IO()
+helloWorld = putStrLn "Hello, World"
+
+-- Hello, Name
+helloName :: IO()
+helloName = do
+    putStrLn "Hey, what is your name?"
+    thisName <- getLine
+    putStrLn ("Hello, " ++ thisName)
+
+-- Magic Eight-Ball Toy
+eightBall :: IO()
+eightBall = do
+    putStrLn "Welcome to the world of Magic Eight-Ball. Ask what your heart wishes..."
+    doesntMatter <- getLine
+    let randNum = fst $ randomR (0, ((length responses) - 1)) (mkStdGen (length doesntMatter))
+    putStrLn (responses !! randNum)
+    where
+        responses = ["Yeppidy Duppidy Dup!", "You got this, homie!", "Fishes food", "Life is beautiful, and so are you", "Keep on trying", "Nah, maybe try some other thing", "42"]
